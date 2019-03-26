6cfeeccdb0361339b37243d792aee0fb826cb76a
Guilherme <guilhermee9@live.com>
2019-03-26 08:56:39 -0300

    completo menos tempo

--- f.1.ino
+++ f.1.ino
@@ -19,106 +19,84 @@ int sinal_verd_ramp = 13;
 //Controle de entrada em funçoes
 int alterado = 0;
 
-  
+//MSG SAIDA
+String msgBotao = "";
+
 void setup()
 {
   Serial.begin(9600); //inicia porta serial
   //Define pinos para o rele como saida
-  pinMode(sinal_verm_est, OUTPUT); 
+  pinMode(sinal_verm_est, OUTPUT);
   pinMode(sinal_verd_est, OUTPUT);
   pinMode(sinal_verm_ramp, OUTPUT);
   pinMode(sinal_verd_ramp, OUTPUT);
   //Corte leitoras
-  pinMode(prox_est, OUTPUT); 
+  pinMode(prox_est, OUTPUT);
   pinMode(prox_ramp, OUTPUT);
   pinMode(prox_ent, OUTPUT);
   pinMode(prox_e_ramp, OUTPUT);
   //Define pinos dos botoes como entrada
-  pinMode(totem_ent, INPUT); 
+  pinMode(totem_ent, INPUT);
   pinMode(totem_ramp, INPUT);
   pinMode(totem_est, INPUT);
   pinMode(totem_e_ramp, INPUT);
-  
+
   //Estado inicial dos reles - desligados
   digitalWrite(sinal_verm_est, HIGH); //vermelho esq
   digitalWrite(sinal_verd_est, LOW); //verde esq
   digitalWrite(sinal_verm_ramp, HIGH); //vermelho dir
   digitalWrite(sinal_verd_ramp, LOW); //verde dir
-  
-
 }
 
-void estadoInicial(){
-    digitalWrite(sinal_verm_est, HIGH); //vermelho esq
-    digitalWrite(sinal_verd_est, LOW); //verde esq
-    digitalWrite(sinal_verm_ramp, HIGH); //vermelho dir
-    digitalWrite(sinal_verd_ramp, LOW); //verde dir
+void estadoInicial() {
+  digitalWrite(sinal_verm_est, HIGH); //vermelho esq
+  digitalWrite(sinal_verd_est, LOW); //verde esq
+  digitalWrite(sinal_verm_ramp, HIGH); //vermelho dir
+  digitalWrite(sinal_verd_ramp, LOW); //verde dir
 }
 
-void delayAuto(){
-  if(alterado == 1){
-    delay(1000*1);
+void delayAuto() {
+  if (alterado == 1) {
+    Serial.println(msgBotao);
+    msgBotao = "";
+    delay(1000*2);
+    estadoInicial();
     alterado = 0;
   }
 }
- 
+
+void setRelay(int svm_est, int svd_est, int svm_ramp, int svd_ramp, int prox_est, int prox_ramp, int prox_e_ramp) {
+  digitalWrite(sinal_verm_est, svm_est);
+  digitalWrite(sinal_verd_est, svd_est);
+  digitalWrite(sinal_verm_ramp, svm_ramp);
+  digitalWrite(sinal_verd_ramp, svd_ramp);
+  digitalWrite(prox_est, prox_est);
+  digitalWrite(prox_ramp, prox_ramp);
+  digitalWrite(prox_e_ramp, prox_e_ramp);
+}
+
 
 void loop()
 {
-  while(digitalRead(totem_ent) == 0){
-  alterado = 1;
-    digitalWrite(sinal_verm_est, HIGH);
-    digitalWrite(sinal_verd_est, LOW);
-    digitalWrite(sinal_verm_ramp, HIGH);
-    digitalWrite(sinal_verd_ramp, LOW);
-    digitalWrite(prox_est, LOW);
-    digitalWrite(prox_ramp, LOW);
-    digitalWrite(prox_e_ramp, LOW);
-    }
-  Serial.println("TOTEM ENTRADA ACIONADO");
-  delayAuto();
-  estadoInicial();
-
-  while(digitalRead(totem_ramp) == 0){
+  if (digitalRead(totem_ent) == 1) {
     alterado = 1;
-    digitalWrite(sinal_verm_est, HIGH);
-    digitalWrite(sinal_verd_est, LOW);
-    digitalWrite(sinal_verm_ramp, LOW);
-    digitalWrite(sinal_verd_ramp, HIGH);
-    digitalWrite(prox_est, LOW);
-    digitalWrite(prox_ent, LOW);
-    digitalWrite(prox_e_ramp, LOW);
-    }
-  Serial.println("TOTEM RAMPA ACIONADO");
-  delayAuto();
-  estadoInicial();
-
-  while(digitalRead(totem_est) == 0){
+    msgBotao = "Totem Entrada";
+    setRelay(HIGH, LOW, HIGH, LOW, LOW, LOW, LOW);
+    delayAuto();
+  }else if (digitalRead(totem_ramp) == 1) {
     alterado = 1;
-    digitalWrite(sinal_verm_est, LOW);
-    digitalWrite(sinal_verd_est, HIGH);
-    digitalWrite(sinal_verm_ramp, HIGH);
-    digitalWrite(sinal_verd_ramp, LOW);
-    digitalWrite(prox_ent, LOW);
-    digitalWrite(prox_ramp, LOW);
-    digitalWrite(prox_e_ramp, LOW);
-    }
-  Serial.println("TOTEM ESTACIONAMENTO ACIONADO");
-  delayAuto();
-  estadoInicial();
-  
-   while(digitalRead(totem_e_ramp) == 0){
+    msgBotao = "Totem Saída Rampa ";
+    setRelay(HIGH,LOW,LOW,HIGH,LOW,LOW,LOW);
+    delayAuto();
+  }else if (digitalRead(totem_est) == 1) {
     alterado = 1;
-    digitalWrite(sinal_verm_est, HIGH);
-    digitalWrite(sinal_verd_est, LOW);
-    digitalWrite(sinal_verm_ramp, HIGH);
-    digitalWrite(sinal_verd_ramp, LOW);
-    digitalWrite(prox_est, LOW);
-    digitalWrite(prox_ramp, LOW);
-    digitalWrite(prox_ent, LOW);
-    }
-  Serial.println("TOTEM ENTRADA RAMPA ACIONADO");
-  delayAuto();
-  estadoInicial();
+    msgBotao = "Totem Saída Estacionamento";
+    setRelay(LOW,HIGH,HIGH,LOW,LOW,LOW,LOW);
+    delayAuto();
+  }else if (digitalRead(totem_e_ramp) == 1) {
+    alterado = 1;
+    msgBotao = "Totem Saída Rampa";
+    setRelay(HIGH,LOW,HIGH,LOW,LOW,LOW,LOW);
+    delayAuto();
+  }
 }
-  
